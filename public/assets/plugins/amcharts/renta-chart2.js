baacbf7c18dd1e9c02427654dfc059b786327e6d
diegosanru <diego.sanchez@irstrat.com>
2025-07-20 14:04:05 -0600

    actualizando sitio con cifras 2T25

--- public/assets/plugins/amcharts/renta-chart2.js
+++ public/assets/plugins/amcharts/renta-chart2.js
@@ -23,7 +23,7 @@ else {
 
 var types = [{
     type: deuda,
-    percent: 1912,
+    percent: 1806,
     color: "#B7CEE5",
     subs: [
         { type: capital, percent: 581 },
@@ -65,7 +65,7 @@ var types = [{
     },
     {
         type: "CADU20V",
-        percent: 493,
+        percent: 494,
         color: "#333960"
         //subs: [
         //    { type: "Hydro", percent: 15 },
