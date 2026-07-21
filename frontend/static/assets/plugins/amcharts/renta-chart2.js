61e3afc699bb9711646aff824a447aa5a0a7fa33
PABLODC99\pablo <pablo.dominguez@irstrat.com>
2026-07-21 11:52:16 -0600

    actualizacion cifras 2T26

--- frontend/static/assets/plugins/amcharts/renta-chart2.js
+++ frontend/static/assets/plugins/amcharts/renta-chart2.js
@@ -23,7 +23,7 @@ else {
 
 var types = [{
     type: deuda,
-    percent: 1794,
+    percent: 2188,
     color: "#B7CEE5",
     subs: [
         { type: capital, percent: 581 },
@@ -65,7 +65,7 @@ var types = [{
     },
     {
         type: "CADU20V",
-        percent: 496,
+        percent: 497,
         color: "#333960"
         //subs: [
         //    { type: "Hydro", percent: 15 },
